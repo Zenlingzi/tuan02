85e6c6def031a19920e9a7c1369656b7657c2d86
Zenlingzi <91784694+Zenlingzi@users.noreply.github.com>
2021-11-18 16:52:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Cậu kia!',
+    introDesc: `Tớ có điều này muốn hỏi, cậu phải trả lời thật lòng không thì đừng trách tại sao tớ yêu cậu`,
+    btnIntro: 'OK',
+    title: 'Cậu yêu tớ có phải không :>',
+    desc: 'Nếu cậu không trả lời mà thoát ra thì mai lên phường ký giấy kết hôn với tớ đấy nhé! ',
+    btnYes: 'Yêu ơi là yêu <3',
+    btnNo: 'Cậu mơ à',
+    question:'Nói cho tớ lý do cậu thích tớ đi :Đ',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Vì cậu là gu của tớ ;)',
+    mess: 'Tớ biết mà 🥰. Yêu cậu 300.000 😘😘',
+    messDesc: 'Tối nay tớ qua đón cậu đi chơi nhaaaaaaaa:v Còn giờ thì chờ gì nữa mafko inbox cho tớ đi nàooooooo.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/100013808409108'
+}
